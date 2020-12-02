7ad56976e4a647f4e1a6c98ebcae2fa9f4f41192
Bellota_x <cbeth.tolentino@gmail.com>
2020-12-02 14:55:24 -0300

    Cambio en el Git Ignore / se agrega Spring y sus dependencias

---
+++ src/main/java/ar/edu/davinci/dvds20202cg6/DvDs20202cG6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20202cG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20202cG6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20202cg6/DvDs20202cG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
